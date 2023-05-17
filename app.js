6e981601d8cfe7e380b7b93f7dfa4d2db925407f
silviacremascoli <silcremascoli@gmail.com>
2023-05-17 14:59:58 +0200

    Added explanatory comments

--- app.js
+++ app.js
@@ -25,10 +25,16 @@ let secretMessage = [
   "JavaScript",
 ];
 
-secretMessage.pop();
+secretMessage.pop(); // removes the last string
 
-secretMessage.push("to", "Program");
+secretMessage.push("to", "Program"); // adds the words "to" and "Program" as separate strings to the end
 
-secretMessage[7] = "right";
+secretMessage[7] = "right"; // replaces the index 7 value with another one
 
-secretMessage.shift();
+secretMessage.shift(); // removes the first string
+
+secretMessage.unshift("Programming"); // adds the value 'Programming' at the beginning of the array
+
+secretMessage.splice(6, 5, "know,"); // removes the strings "get", "right", "the", "first", "time,", and replace them with the single string "know,"
+
+console.log(secretMessage.join(" ")); // joins the elements of the array into a string
